5d86af2492bccd4985bd0676924b91c3c0a0ee58
Varsha Rawat <varsha.rawat690@gmail.com>
2022-11-08 16:37:19 +0530

    Cleanup file

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,17 @@ module.exports = {
     es2021: true
   },
   extends: 'standard',
+  plugins: ['react'],
   overrides: [
   ],
   parserOptions: {
     ecmaVersion: 'latest'
   },
   rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'linebreak-style': 'off',
+    semi: ['error', 'always']
+
   }
-}
+};
